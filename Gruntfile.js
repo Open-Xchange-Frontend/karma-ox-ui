21d66cb92d7aefc00204184bd63bd05deaf486b1
Julian Bäume <julian@svg4all.de>
2014-07-15 18:09:52 +0200

    add grunt-bump to help releasing new versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,16 @@
 'use strict';
 
 module.exports = function (grunt) {
+    grunt.config.merge({
+        bump: {
+            options: {
+                createTag: false,
+                push: false
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-bump');
+
     grunt.registerTask('default', []);
 };
 
